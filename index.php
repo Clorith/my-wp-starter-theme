749919ba5c429f64f6c89b88d90c44438fc5a3a5
marius <funman@gmail.com>
2014-03-25 09:27:39 +0100

    Fixed text domai nreference and non-english text

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
 <?php while ( have_posts() ) : the_post() ?>
 	<?php the_title('<h2>','</h2>'); ?>
-	<?php the_content( __( 'Les mer <span class="meta-nav">&raquo;</span>', 'amet' )  ); ?>
+	<?php the_content( __( 'Read more <span class="meta-nav">&raquo;</span>', 'text-domain' )  ); ?>
 <?php endwhile; ?>
 
 <?php
